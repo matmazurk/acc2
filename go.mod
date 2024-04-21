c99e59c7889c0f229c083ab20925d202778d9336
Mateusz Mazurkiewicz <mateusz.mazurkiewicz@karhoo.com>
2024-04-21 08:04:28 +0200

    add flag to main, start http server in separate goroutine

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.22.0
 require (
 	github.com/google/uuid v1.6.0
 	github.com/pkg/errors v0.9.1
+	github.com/rs/zerolog v1.32.0
 	github.com/stretchr/testify v1.9.0
 	gorm.io/driver/sqlite v1.5.5
 	gorm.io/gorm v1.25.9
@@ -14,7 +15,10 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-sqlite3 v1.14.17 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,19 +1,33 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"flag"
 	"fmt"
-	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/matmazurk/acc2/db"
 	lhttp "github.com/matmazurk/acc2/http"
 	"github.com/matmazurk/acc2/imagestore"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
 const listenAddr = ":80"
 
 func main() {
-	fmt.Println("starting...")
+	flags := parseFlags()
+	cleanup := setup(flags)
+	defer cleanup()
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	log.Info().Msg("starting...")
 	db, err := db.New("exps.db")
 	if err != nil {
 		panic(err)
@@ -22,10 +36,64 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	mux := lhttp.NewMux(db, store)
-	log.Println("listening on", listenAddr)
-	err = http.ListenAndServe(listenAddr, mux)
+	server := &http.Server{
+		Addr:    listenAddr,
+		Handler: lhttp.NewMux(db, store),
+	}
+
+	go func() {
+		log.Info().Str("listen_addr", listenAddr).Msg("starting http server")
+		err := server.ListenAndServe()
+		if err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Fatal().Err(err).Msg("error http server listen")
+		}
+	}()
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+	<-sigCh
+	fmt.Println()
+
+	log.Info().Msg("shutting down http server...")
+	err = server.Shutdown(ctx)
 	if err != nil {
-		panic(err)
+		log.Fatal().Err(err).Msg("error shutting down http server")
+	}
+
+	log.Info().Msg("http server gracefully shutdown")
+
+	cancel()
+}
+
+type flags struct {
+	printToStdout bool
+}
+
+func parseFlags() flags {
+	f := flags{}
+	flag.BoolVar(&f.printToStdout, "s", false, "print output to stdout")
+	flag.Parse()
+
+	return f
+}
+
+func setup(f flags) func() {
+	var callbacks []func()
+
+	if f.printToStdout {
+		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339})
+	} else {
+		f, err := os.OpenFile("logs", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0o600)
+		if err != nil {
+			panic(err)
+		}
+		log.Logger = log.Output(f)
+		callbacks = append(callbacks, func() { f.Close() })
+	}
+
+	return func() {
+		for _, c := range callbacks {
+			c()
+		}
 	}
 }
