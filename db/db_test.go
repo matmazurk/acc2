a5a0b1caa2aefc982086c29f03ece757e618af9d
matmazurk <mat.mazurk@gmail.com>
2024-10-24 17:07:15 +0200

    adjust db tests; add db migration in factory function

--- db/db_test.go
+++ db/db_test.go
@@ -1,75 +1,44 @@
 package db_test
 
 import (
-	"os"
 	"slices"
 	"testing"
 	"time"
 
-	"github.com/golang-migrate/migrate/v4"
-	_ "github.com/golang-migrate/migrate/v4/database/sqlite"
-	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/google/uuid"
 	"github.com/matmazurk/acc2/db"
 	"github.com/matmazurk/acc2/model"
-	"github.com/pkg/errors"
-	"github.com/rs/zerolog"
 	"github.com/stretchr/testify/require"
 )
 
-var (
-	client db.Client
-)
-
-func TestMain(m *testing.M) {
-	file, err := os.OpenFile(".test.db", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
-	if err != nil {
-		panic(err)
-	}
-	file.Close()
-
-	migrations, err := migrate.New("file://../migrations", "sqlite://"+file.Name())
-	if err != nil {
-		panic(err)
-	}
-	err = migrations.Up()
-	if err != nil && !errors.Is(err, migrate.ErrNoChange) {
-		panic(err)
-	}
-
-	client, err = db.New(file.Name())
-	if err != nil {
-		panic(err)
-	}
-
-	os.Exit(m.Run())
-}
-
-var logger = zerolog.New(zerolog.Nop())
+const dbFile = ".test.db"
 
 func TestDB(t *testing.T) {
+	c, err := db.New(dbFile)
+	require.NoError(t, err)
+
 	payer := uuid.NewString()
 	category := uuid.NewString()
 
 	t.Run("should_properly_insert_list_payers", func(t *testing.T) {
-		err := client.CreatePayer(payer)
+		err := c.CreatePayer(payer)
 		require.NoError(t, err)
-		err = client.CreatePayer(payer)
+		err = c.CreatePayer(payer)
 		require.Error(t, err)
 
-		payers, err := client.ListPayers()
+		payers, err := c.ListPayers()
 		require.NoError(t, err)
 		idx := slices.IndexFunc(payers, func(name string) bool { return name == payer })
 		require.NotEqual(t, -1, idx)
 	})
 
 	t.Run("should_properly_insert_list_categories", func(t *testing.T) {
-		err := client.CreateCategory(category)
+		err := c.CreateCategory(category)
 		require.NoError(t, err)
-		err = client.CreateCategory(category)
+		err = c.CreateCategory(category)
 		require.Error(t, err)
 
-		categories, err := client.ListCategories()
+		categories, err := c.ListCategories()
 		require.NoError(t, err)
 		idx := slices.IndexFunc(categories, func(name string) bool { return name == category })
 		require.NotEqual(t, -1, idx)
@@ -86,7 +55,7 @@ func TestDB(t *testing.T) {
 			CreatedAt:   now,
 		}.Build()
 		require.NoError(t, err)
-		err = client.Insert(exp1)
+		err = c.Insert(exp1)
 		require.NoError(t, err)
 
 		exp2, err := model.ExpenseBuilder{
@@ -98,7 +67,7 @@ func TestDB(t *testing.T) {
 			CreatedAt:   now.Add(time.Minute),
 		}.Build()
 		require.NoError(t, err)
-		err = client.Insert(exp2)
+		err = c.Insert(exp2)
 		require.NoError(t, err)
 
 		exp3, err := model.ExpenseBuilder{
@@ -110,7 +79,7 @@ func TestDB(t *testing.T) {
 			CreatedAt:   now.Add(time.Hour),
 		}.Build()
 		require.NoError(t, err)
-		err = client.Insert(exp3)
+		err = c.Insert(exp3)
 		require.NoError(t, err)
 
 		expectedOrder := []model.Expense{
@@ -119,7 +88,7 @@ func TestDB(t *testing.T) {
 			exp1,
 		}
 
-		exps, err := client.SelectExpenses()
+		exps, err := c.SelectExpenses()
 		require.NoError(t, err)
 
 		filteredExps := filterExpenses(exps, exp1.ID(), exp2.ID(), exp3.ID())
@@ -142,18 +111,18 @@ func TestDB(t *testing.T) {
 			CreatedAt:   now,
 		}.Build()
 		require.NoError(t, err)
-		err = client.Insert(exp)
+		err = c.Insert(exp)
 		require.NoError(t, err)
 
-		exps, err := client.SelectExpenses()
+		exps, err := c.SelectExpenses()
 		require.NoError(t, err)
 		idx := slices.IndexFunc(exps, func(e model.Expense) bool { return e.ID() == exp.ID() })
 		require.Positive(t, idx)
 
-		err = client.RemoveExpense(exp)
+		err = c.RemoveExpense(exp)
 		require.NoError(t, err)
 
-		exps, err = client.SelectExpenses()
+		exps, err = c.SelectExpenses()
 		require.NoError(t, err)
 		idx = slices.IndexFunc(exps, func(e model.Expense) bool { return e.ID() == exp.ID() })
 		require.Equal(t, -1, idx)
